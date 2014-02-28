b11bcb4e4f1382c0d8ceaa93f352d4385e5461ed
Filip Oscadal <filip@mxd.cz>
2014-02-28 03:12:47 +0100

    first version

---
+++ create-blocklist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curl https://www.iblocklist.com/lists.php  | grep -Po "http:\/\/list.iblocklist.com/\?list=[^']*" > file_list
+wget -c -i file_list
+
+j=1
+for i in *archiveformat=gz; do mv $i $j.txt.gz; j=$(($j+1)); done
+
+gunzip *.gz
+
+cat *.txt | uniq > blocklist
+
+LINES=`cat blocklist | wc -l`
+echo -e "\nBlocklist created: $LINES lines\n"
+ls -l blocklist
+
+rm *.txt
+
+exit 0
